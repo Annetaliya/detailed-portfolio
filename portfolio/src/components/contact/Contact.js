9199610f94eec042d2e7293face66ca24a81f87b
Annette Adhiambo <annetaliya@gmail.com>
2023-12-30 00:21:05 -0800

    emailjs

--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './contact.css';
 import FacebookIcon from '../../assets/facebook-icon.png';
-import Facebook from '../../assets/facebook.png';
 import Twitter from '../../assets/twitter.png';
 import Instagram from '../../assets/instagram.png';
 import Youtube from '../../assets/youtube.png';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_9nzarkq', 'template_sf7srco', form.current, 'il4SILaCnHiEQ2JNk')
+          .then((result) => {
+              console.log(result.text);
+              e.target.reset();
+              alert('Email sent');
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
     return (
         <section id='contactPage'>
             <div id='contact'>
@@ -14,9 +28,9 @@ const Contact = () => {
                 <span className='contactDesc'>
                     Please fill out the form below to discuss any work opprtunities
                 </span>
-                <form className='contactForm'>
-                    <input type='text' className='name' placeholder='Your Name'/>
-                    <input type='email' className='email' placeholder='Your email'/>
+                <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                    <input type='text' className='name' placeholder='Your Name' name='your_name'/>
+                    <input type='email' className='email' placeholder='Your email' name='your_email'/>
                     <textarea className='msg' name='message' rows='5'placeholder='Your message'></textarea>
                     <button type='submit' value='send' className="submitBtn">Submit</button>
                     <div className='links'>
