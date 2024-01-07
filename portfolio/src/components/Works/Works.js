62de5dd612c89a5b4afc722ef63858fea7af565e
Annette Adhiambo <annetaliya@gmail.com>
2024-01-06 18:13:56 -0800

    Added external link

--- portfolio/src/components/Works/Works.js
+++ portfolio/src/components/Works/Works.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import './works.css';
-import Portfolio1 from '../../assets/portfolio-1.png';
-import Portfolio2 from '../../assets/portfolio-2.png';
-import Portfolio3 from '../../assets/portfolio-3.png';
-import Portfolio4 from '../../assets/portfolio-4.png';
-import Portfolio5 from '../../assets/portfolio-5.png';
-import Portfolio6 from '../../assets/portfolio-6.png';
+import Portfolio1 from '../../assets/web screen.PNG';
+
+
 
 function Works() {
+    const mySite = 'https://659968189ed589e081611c57--jocular-valkyrie-82c7de.netlify.app/'
     return (
         <section id="works">
             <h2 className='worksTitle'> My Portfolio</h2>
             <span className='worksDesc'>I take pride in paying attention to the smallest details and making sure my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals</span>
             <div className='worksImgs'>
-                <img src={Portfolio1} alt="" className='worksImg'/>
-                <img src={Portfolio2} alt="" className='worksImg'/>
-                <img src={Portfolio3} alt="" className='worksImg'/>
-                <img src={Portfolio4} alt="" className='worksImg'/>
-                <img src={Portfolio5} alt="" className='worksImg'/>
-                <img src={Portfolio6} alt="" className='worksImg'/>
+                <h2 className='worksTitle'>Click the image below to follow the link</h2>
+                <a href={mySite} target="_blank" rel="noopener noreferrer">
+                    <img src={Portfolio1} alt="" className='worksImg'/> 
+
+                </a>
 
             </div>
             <button className='worksBtn'>See more</button>
